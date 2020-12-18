23b3145962eb7846ed1ca69b50d439ff504dfc21
PatriotZest <49161627+PatriotZest@users.noreply.github.com>
2020-12-18 10:24:31 +0530

    ok

---
+++ main.js
@@ -0,0 +1,104 @@
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+
+const { token, default_prefix } = require('./config.json');
+
+const { readdirSync } = require('fs');
+
+const { join } = require('path');
+
+const { GiveawaysManager } = require('discord-giveaways');
+
+const ms = require('ms');
+
+const config = require('./config.json');
+client.config = config;
+const antiAd = require('./anti-ad')
+const warn = require('./commands/warn')
+const db = require('quick.db')
+
+antiAd(client)
+
+client.commands= new Discord.Collection();
+//You can change the prefix if you like. It doesn't have to be ! or ;
+const commandFiles = readdirSync(join(__dirname, "commands")).filter(file => file.endsWith(".js"));
+
+for (const file of commandFiles) {
+    const command = require(join(__dirname, "commands", `${file}`));
+    client.commands.set(command.name, command);
+}
+
+
+client.on("error", console.error);
+
+client.on('ready', () => {
+    console.log("yes its online");
+    client.user.setActivity(`ur mom`,{
+         type: "STREAMING",
+         url: "https://www.twitch.tv/patriotzest"})
+    
+});
+client.giveawaysManager = new GiveawaysManager(client, {
+    storage: "./giveaways.json",
+    updateCountdownEvery: 5000,
+    default: {
+        botsCanWin: false,
+        exemptPermissions: ["MANAGE_MESSAGES", "ADMINISTRATOR"],
+        embedColor: "#FF0000",
+        reaction: "🎉"
+    }
+});
+
+
+
+
+
+
+
+
+
+
+
+client.on("message", async message => {
+
+    if(message.author.bot) return;
+if(db.has(`afk-${message.author.id}+${message.guild.id}`)) {
+        const info = db.get(`afk-${message.author.id}+${message.guild.id}`)
+        await db.delete(`afk-${message.author.id}+${message.guild.id}`)
+        message.reply(`Your afk status have been removed (${info})`)
+    }
+
+
+    if(message.mentions.members.first()) {
+        if(db.has(`afk-${message.mentions.members.first().id}+${message.guild.id}`)) {
+            message.channel.send(message.mentions.members.first().user.tag + ":" + db.get(`afk-${message.mentions.members.first().id}+${message.guild.id}`))
+        }else return;
+    }else;
+    if(message.channel.type === 'dm') return;
+
+    if(message.content.startsWith(default_prefix)) {
+        const args = message.content.slice(default_prefix.length).trim().split(/ +/);
+
+        const command = args.shift().toLowerCase();
+
+        if(!client.commands.has(command)) return;
+
+
+        try {
+            client.commands.get(command).run(client, message, args);
+
+        } catch (error){
+            console.error(error);
+        }
+    }
+}) 
+
+
+
+const usersMap = new Map();
+const LIMIT = 5;
+const TIME = 7000;
+const DIFF = 3000;
+
+client.login(token);
